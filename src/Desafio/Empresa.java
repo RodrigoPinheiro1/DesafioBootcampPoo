0ca6c35aa4ed3749c38ae6f3ba7fc793a9b61b4e
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2022-01-28 16:57:44 -0300

    DesafioBootcamp

---
+++ src/Desafio/Empresa.java
@@ -0,0 +1,35 @@
+package Desafio;
+
+public class Empresa
+
+{
+
+    private String nomeEmpresa;
+    private int vagas;
+    private String descricaoEmpresa;
+
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public int getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(int vagas) {
+        this.vagas = vagas;
+    }
+
+    public String getDescricaoEmpresa() {
+        return descricaoEmpresa;
+    }
+
+    public void setDescricaoEmpresa(String descricaoEmpresa) {
+        this.descricaoEmpresa = descricaoEmpresa;
+    }
+}
